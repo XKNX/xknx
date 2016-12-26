fe803a5849d08940413bde1fc651a5d78f7869b9
twistermcneal <info@pc-edv-technik.de>
2016-12-26 22:24:56 +0100

    Update conversion.py

--- xknx/conversion.py
+++ xknx/conversion.py
@@ -2,7 +2,7 @@ from datetime import time, date, datetime, timedelta
 from .colors import Colors
 from enum import Enum
 
-class xKNXCouldNotConvert(Exception):
+class CouldNotConvert(Exception):
     def __init__(self):
         pass
 
@@ -10,16 +10,18 @@ class xKNXCouldNotConvert(Exception):
         return "CouldNotConvert"
 
 class DPT():
-    def __init__(self, type = "", name = "", valuerange= (0,100), unit="", bitlength = 1):	
-	    self._type = type
+    def __init__(self, dptype = "", name = "", valuerange= (0,1), unit="", bitlength = 1):	
+	    self._dptype = dptype
 	    self._name = name
 	    self._valuerange = valuerange
 	    self._unit  = unit
 	    self._bitlength = bitlength
+	    self._value = 0
+	    self._raw = [0]
     @property
-    def type(self):
+    def dptype(self):
 	    """Return the type"""
-	    return _type		
+	    return _dptype		
     @property
     def name(self):
 	    """Return the name"""
@@ -33,27 +35,34 @@ class DPT():
 	    """Return the unit"""
 	    return _unit	
     @property
+    def value(self):
+	    """Return the value"""
+	    return _value	
+    @property
+    def raw(self):
+	    """Return the rawknxdata"""
+	    return _raw	
+    @property
     def bitlength(self):
-	    """Return the unit"""
-	    return _bitlength			
+	    """Return the bitlength"""
+	    return _bitlength		
 		
-class DPTTypes(Enum):
-    DPT_1_Switch  = DPT("1.001", "Switch", (0, 1), "", 1)
-    DPT_3_Dimming  = DPT("3.000", "Dimming", ([0,0], [1,7]), "", 8)
-    DPT_4_ASCII_CHAR  = DPT("4.000", "ASCII_CHAR", (0, 1), "", 8)
-    DPT_5_Scaling = DPT("5.001", "Scaling", (0, 100), "%")
-    DPT_5_Angle = DPT("5.003", "Angle", (0, 360), "s")
-    DPT_5_Percent_U8 = DPT("5.004", "Percent (8 bit)", (0, 255), "%")
-    DPT_5_DecimalFactor = DPT("5.005", "Decimal factor", (0, 1), "ratio")
-    DPT_7_INT_Unsigned  = DPT("7.000", "Int unigned", (0, 65535), "", 16)
-    DPT_8_INT_Signed  = DPT("8.000", "Int signed", (-32768, 32767), "", 16)
-    DPT_9_FLOAT  = DPT("9.000", "Foat", (-671088.64, 670760.96), "", 16)
+    def convert2knx(self, value):
+        pass
+    def convert2value(self, knxdata):
+        pass
+
 	
-class xKNXconvert():
-    def float2knx(value):
+class DPT_9_FLOAT(DPT):
+    def __init__(self,  value = None,  data = None):
+        DPT.__init__( "9.000", "Float", (-671088.64, 670760.96), "",  16)	   
+        self._value = value
+        self._raw = data
+		
+    def convert2knxdata(value):
         """Convert a float to a 2 byte KNX float value"""
         if value < -671088.64 or value > 670760.96:
-            raise xKNXCouldNotConvert("float {} out of valid range".format(value))
+            raise CouldNotConvert("DPT_9 {} out of valid range".format(value))
 			
         value = value * 100
         i = 0
@@ -70,15 +79,14 @@ class xKNXconvert():
 
         return [(sign << 7) + (i << 3) + (mantisse >> 8),
             mantisse & 0xff]
-			
-    def knx2float(knxdata):
+
+    def convert2value(knxdata):
         """Convert a KNX 2 byte float object to a float"""
         if knxdata == None:
-          raise xKNXCouldNotConvert("Can not convert a object of Type None")
+          raise CouldNotConvert("Can not convert a object of Type None")
         if len(knxdata) != 2:
-          raise xKNXCouldNotConvert("Can only convert a 2 Byte object to float")
-	  
-      
+          raise CouldNotConvert("Can only convert a 2 Byte object to float")
+	       
         data = knxdata[0] * 256 + knxdata[1]
         sign = data >> 15
         exponent = (data >> 11) & 0x0f
