f7f6820e9bcb122b9a34c1d48411e47792a3094b
Julius Mittenzwei <julius@mittenzwei.com>
2017-01-01 19:58:31 +0100

    typo

--- home-assistant-plugin/custom_components/xknx/xknx_wrapper.py
+++ home-assistant-plugin/custom_components/xknx/xknx_wrapper.py
@@ -30,7 +30,7 @@ class XKNX_Wrapper(object):
 
         Config(self.xknx).read(file=self.config_file)
 
-        xknx.start( telegram_received_callback = self.telegram_received_callback )
+        self.xknx.start( telegram_received_callback = self.telegram_received_callback )
 
         self.hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, self.stop)
 
