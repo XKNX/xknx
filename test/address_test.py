acfed0582bb02254c2addb71afd785b0ba53e321
Julius Mittenzwei <julius@mittenzwei.com>
2017-01-06 14:02:18 +0100

    typo

--- test/address_test.py
+++ test/address_test.py
@@ -78,17 +78,17 @@ class TestAddress(unittest.TestCase):
     # ADDRESS TYPE
     #
     def test_address_type_physical(self):
-        self.assertTrue(
+        self.assertEqual(
             Address("2.3.4", AddressType.PHYSICAL).address_type,
             AddressType.PHYSICAL)
 
     def test_address_type_physical_auto_detect(self):
-        self.assertTrue(
+        self.assertEqual(
             Address("2.3.4").address_type,
             AddressType.PHYSICAL)
 
     def test_address_type_group(self):
-        self.assertTrue(
+        self.assertEqual(
             Address("2/3/4").address_type,
             AddressType.GROUP)
 
