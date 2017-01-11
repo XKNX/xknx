bf823e5d60170df54f868b3605bfd877eb0cd0f1
Julius Mittenzwei <julius@mittenzwei.com>
2017-01-11 21:37:01 +0100

    better handling of exceptions while telegrams are processed

--- xknx/telegram_processor.py
+++ xknx/telegram_processor.py
@@ -18,17 +18,20 @@ class TelegramProcessor(threading.Thread):
             telegram = self.xknx.telegrams.get()
             self.process_telegram(telegram)
             self.xknx.telegrams.task_done()
-
             if telegram.direction == TelegramDirection.OUTGOING:
                 # limit rate to knx bus to 20 per second
                 time.sleep(1/20)
 
-
     def process_telegram(self, telegram):
-        if telegram.direction == TelegramDirection.INCOMING:
-            self.process_telegram_incoming(telegram)
-        elif telegram.direction == TelegramDirection.OUTGOING:
-            self.process_telegram_outgoing(telegram)
+        try:
+
+            if telegram.direction == TelegramDirection.INCOMING:
+                self.process_telegram_incoming(telegram)
+            elif telegram.direction == TelegramDirection.OUTGOING:
+                self.process_telegram_outgoing(telegram)
+
+        except Exception as exception:
+            print("Exceptio while processing telegram:", exception)
 
 
     def process_telegram_outgoing(self, telegram):
@@ -37,14 +40,11 @@ class TelegramProcessor(threading.Thread):
 
 
     def process_telegram_incoming(self, telegram):
-        try:
-            device = self.xknx.devices.device_by_group_address(
-                telegram.group_address)
-            device.process(telegram)
-            if self.telegram_received_callback:
-                self.telegram_received_callback(self.xknx, device, telegram)
-        except CouldNotResolveAddress as couldnotresolveaddress:
-            print(couldnotresolveaddress)
+        device = self.xknx.devices.device_by_group_address(
+            telegram.group_address)
+        device.process(telegram)
+        if self.telegram_received_callback:
+            self.telegram_received_callback(self.xknx, device, telegram)
 
 
     @staticmethod
