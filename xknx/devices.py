439eaa0bb6d6eb1f048ff97d816c6b1ce8f2f7dd
Julius Mittenzwei <julius@mittenzwei.com>
2017-01-14 12:11:13 +0100

    made _devices object private

--- xknx/devices.py
+++ xknx/devices.py
@@ -3,31 +3,31 @@ from .device import Device
 class Devices:
 
     def __init__(self):
-        self._devices = []
+        self.__devices = []
 
 
     def __iter__(self):
-        yield from self._devices
+        yield from self.__devices
 
 
     def devices_by_group_address(self, group_address):
-        for device in self._devices:
+        for device in self.__devices:
             if device.has_group_address(group_address):
                 yield device
 
 
     def __getitem__(self, key):
-        for device in self._devices:
+        for device in self.__devices:
             if device.name == key:
                 return device
         raise KeyError
 
 
     def __len__(self):
-        return len(self._devices)
+        return len(self.__devices)
 
 
     def add(self, device):
         if not isinstance(device, Device):
             raise TypeError()
-        self._devices.append(device)
+        self.__devices.append(device)
