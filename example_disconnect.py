e3e78bd0bba139ccf99c003d0758a237dfc08a8c
Julius Mittenzwei <julius@mittenzwei.com>
2017-03-20 20:18:37 +0100

    disconnect example is using gateway scanner to find ip

--- example_disconnect.py
+++ example_disconnect.py
@@ -1,9 +1,21 @@
 import asyncio
 from xknx import XKNX
-from xknx.io.async import ConnectionState,Disconnect
-from xknx.io.async import UDPClient
+from xknx.io.async import ConnectionState, Disconnect
+from xknx.io.async import UDPClient, GatewayScanner
 
 xknx = XKNX()
+
+gatewayscanner = GatewayScanner(xknx)
+gatewayscanner.start()
+gatewayscanner.stop()
+
+if not gatewayscanner.found:
+    raise Exception("No Gateways found")
+
+print("Connecting to {}:{} from {}".format(
+    gatewayscanner.found_ip_addr,
+    gatewayscanner.found_port,
+    gatewayscanner.found_local_ip))
 
 own_ip="192.168.42.1"
 gateway_ip="192.168.42.10"
@@ -11,8 +23,8 @@ gateway_port=3671
 
 udp_client = UDPClient(
     xknx,
-    (own_ip,0),
-    (gateway_ip, gateway_port))
+    (gatewayscanner.found_local_ip, 0),
+    (gatewayscanner.found_ip_addr, gatewayscanner.found_port))
 
 task = asyncio.Task(
     udp_client.connect())
