90f3247cde9375d134d13357029c9e940e29d388
Julius Mittenzwei <julius@mittenzwei.com>
2017-07-22 12:33:40 +0200

    pylint

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='xknx',
-        description='A Wrapper around KNX protocol. Documentation: http://xknx.io/',
+    name='xknx',
+    description='A Wrapper around KNX protocol. Documentation: http://xknx.io/',
 
-        version='0.5.0',
-        download_url='https://github.com/XKNX/xknx/archive/0.5.0.zip',
-        url='http://xknx.io/',
+    version='0.5.0',
+    download_url='https://github.com/XKNX/xknx/archive/0.5.0.zip',
+    url='http://xknx.io/',
 
-        author='Julius Mittenzwei',
-        author_email='julius@mittenzwei.com',
-        license='MIT',
-        classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: End Users/Desktop',
-          'Intended Audience :: Developers',
-          'Topic :: System :: Hardware :: Hardware Drivers',
-          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5'
-        ],
-        packages=find_packages(),
-        install_requires=['PyYAML','netifaces'],
-        keywords = 'knx ip knxip eib home automation',
-        zip_safe=False)
+    author='Julius Mittenzwei',
+    author_email='julius@mittenzwei.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    packages=find_packages(),
+    install_requires=['PyYAML', 'netifaces'],
+    keywords='knx ip knxip eib home automation',
+    zip_safe=False)
