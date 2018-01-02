a1e91790785aee9b5f1b1a40a5e6e075b47ca0f9
Martin Sträng <martin@tilljorden.com>
2018-01-02 10:31:31 +0100

    Added support for 4byte counters/values (signed and unsigned), DPT 12.x and DPT 13.x.

--- xknx/knx/__init__.py
+++ xknx/knx/__init__.py
@@ -11,10 +11,12 @@ from .address import Address, AddressType, AddressFormat
 from .address_filter import AddressFilter
 from .telegram import Telegram, TelegramDirection, TelegramType
 from .dpt import DPTBase, DPTBinary, DPTArray, DPTComparator
-from .dpt_float import DPTFloat, DPTIEEE754, DPTLux, DPTTemperature, DPTHumidity, DPTWsp, DPTElectricPotential, DPTElectricCurrent, DPTPower
+from .dpt_float import DPTFloat, DPTIEEE754, DPTLux, DPTTemperature, \
+    DPTHumidity, DPTWsp, DPTElectricPotential, DPTElectricCurrent, DPTPower
 from .dpt_hvac_mode import HVACOperationMode, DPTHVACMode, \
     DPTControllerStatus
 from .dpt_2byte import DPTUElCurrentmA
+from .dpt_4byte import DPT4ByteUnsigned, DPT4ByteSigned
 from .dpt_scaling import DPTScaling
 from .dpt_time import DPTTime, DPTWeekday
 from .dpt_string import DPTString
