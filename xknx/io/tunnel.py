d6ab2ceda71a7039371e4fda82b551c36835a88f
Matthias Urlichs <matthias@urlichs.de>
2018-05-27 05:54:41 +0200

    add default for local IP

--- xknx/io/tunnel.py
+++ xknx/io/tunnel.py
@@ -21,7 +21,7 @@ class Tunnel():
 
     # pylint: disable=too-many-instance-attributes
 
-    def __init__(self, xknx, src_address, local_ip, gateway_ip, gateway_port, telegram_received_callback=None):
+    def __init__(self, xknx, src_address, local_ip="0.0.0.0", gateway_ip=None, gateway_port=3671, telegram_received_callback=None):
         """Initialize Tunnel class."""
         # pylint: disable=too-many-arguments
         self.xknx = xknx
