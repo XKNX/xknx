53723e090cc88b9b0b2d2973f0b390a0ff960673
Julius Mittenzwei <julius@mittenzwei.com>
2018-12-26 13:12:26 +0100

    bumped version

--- home-assistant-plugin/custom_components/xknx.py
+++ home-assistant-plugin/custom_components/xknx.py
@@ -17,7 +17,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import async_track_state_change
 from homeassistant.helpers.script import Script
 
-REQUIREMENTS = ['xknx==0.8.5']
+REQUIREMENTS = ['xknx==0.9.3']
 
 DOMAIN = "xknx"
 DATA_XKNX = "data_knx"
