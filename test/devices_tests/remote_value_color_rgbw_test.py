baa01bb266cebd7e18bb2288c1a307e840db7504
Daniel Strigl <strigl.daniel@gmail.com>
2019-09-22 20:47:41 +0200

    Fix for wrong structure of DPT 251.600 (#231)

--- test/devices_tests/remote_value_color_rgbw_test.py
+++ test/devices_tests/remote_value_color_rgbw_test.py
@@ -26,32 +26,32 @@ class TestRemoteValueColorRGBW(unittest.TestCase):
         remote_value = RemoteValueColorRGBW(xknx)
         input_list = [100, 101, 102, 127]
         input_tuple = (100, 101, 102, 127)
-        expected = DPTArray((0x00, 0x0f, 0x64, 0x65, 0x66, 0x7f))
+        expected = DPTArray((0x64, 0x65, 0x66, 0x7f, 0x00, 0x0f))
         self.assertEqual(remote_value.to_knx(input_tuple), expected)
         self.assertEqual(remote_value.to_knx(input_list), expected)
-        self.assertEqual(remote_value.to_knx((15,) + input_tuple), expected)
-        self.assertEqual(remote_value.to_knx([15] + input_list), expected)
-        self.assertEqual(remote_value.to_knx((0, 15) + input_tuple), expected)
-        self.assertEqual(remote_value.to_knx([0, 15] + input_list), expected)
+        self.assertEqual(remote_value.to_knx(input_tuple + (15,)), expected)
+        self.assertEqual(remote_value.to_knx(input_list + [15]), expected)
+        self.assertEqual(remote_value.to_knx(input_tuple + (0, 15)), expected)
+        self.assertEqual(remote_value.to_knx(input_list + [0, 15]), expected)
 
     def test_from_knx(self):
         """Test from_knx function with normal operation."""
         xknx = XKNX(loop=self.loop)
         remote_value = RemoteValueColorRGBW(xknx)
         self.assertEqual(
-            remote_value.from_knx(DPTArray((0x00, 0x00, 0x64, 0x65, 0x66, 0x7f))),
+            remote_value.from_knx(DPTArray((0x64, 0x65, 0x66, 0x7f, 0x00, 0x00))),
             [0, 0, 0, 0])
         self.assertEqual(
-            remote_value.from_knx(DPTArray((0x00, 0x0f, 0x64, 0x65, 0x66, 0x7f))),
+            remote_value.from_knx(DPTArray((0x64, 0x65, 0x66, 0x7f, 0x00, 0x0f))),
             [100, 101, 102, 127])
         self.assertEqual(
-            remote_value.from_knx(DPTArray((0x00, 0x00, 0x64, 0x65, 0x66, 0x7f))),
+            remote_value.from_knx(DPTArray((0x64, 0x65, 0x66, 0x7f, 0x00, 0x00))),
             [100, 101, 102, 127])
         self.assertEqual(
-            remote_value.from_knx(DPTArray((0x00, 0x09, 0xff, 0x65, 0x66, 0xff))),
+            remote_value.from_knx(DPTArray((0xff, 0x65, 0x66, 0xff, 0x00, 0x09))),
             [255, 101, 102, 255])
         self.assertEqual(
-            remote_value.from_knx(DPTArray((0x00, 0x01, 0x64, 0x65, 0x66, 0x7f))),
+            remote_value.from_knx(DPTArray((0x64, 0x65, 0x66, 0x7f, 0x00, 0x01))),
             [255, 101, 102, 127])
 
     def test_to_knx_error(self):
@@ -90,7 +90,7 @@ class TestRemoteValueColorRGBW(unittest.TestCase):
             telegram,
             Telegram(
                 GroupAddress('1/2/3'),
-                payload=DPTArray((0x00, 0x0f, 0x64, 0x65, 0x66, 0x67))))
+                payload=DPTArray((0x64, 0x65, 0x66, 0x67, 0x00, 0x0f))))
         self.loop.run_until_complete(asyncio.Task(remote_value.set((100, 101, 104, 105))))
         self.assertEqual(xknx.telegrams.qsize(), 1)
         telegram = xknx.telegrams.get_nowait()
@@ -98,7 +98,7 @@ class TestRemoteValueColorRGBW(unittest.TestCase):
             telegram,
             Telegram(
                 GroupAddress('1/2/3'),
-                payload=DPTArray((0x00, 0x0f, 0x64, 0x65, 0x68, 0x69))))
+                payload=DPTArray((0x64, 0x65, 0x68, 0x69, 0x00, 0x0f))))
 
     def test_process(self):
         """Test process telegram."""
@@ -108,7 +108,7 @@ class TestRemoteValueColorRGBW(unittest.TestCase):
             group_address=GroupAddress("1/2/3"))
         telegram = Telegram(
             group_address=GroupAddress("1/2/3"),
-            payload=DPTArray((0x00, 0x0f, 0x64, 0x65, 0x66, 0x67)))
+            payload=DPTArray((0x64, 0x65, 0x66, 0x67, 0x00, 0x0f)))
         self.loop.run_until_complete(asyncio.Task(remote_value.process(telegram)))
         self.assertEqual(remote_value.value, [100, 101, 102, 103])
 
