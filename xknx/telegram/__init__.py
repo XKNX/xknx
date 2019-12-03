071a8283703a245a7390e3abc5a4f6b2483859e2
Bas Stottelaar <basstottelaar@gmail.com>
2019-12-03 19:52:44 +0100

    Add `xknx.telegram.apci`
    Thi adds payload classes that represent APCI messages, such as
    GroupValueRead/Write/Response.

--- xknx/telegram/__init__.py
+++ xknx/telegram/__init__.py
@@ -8,4 +8,5 @@ Module for handling KNX primitves.
 # flake8: noqa
 from .address import GroupAddress, GroupAddressType, IndividualAddress
 from .address_filter import AddressFilter
+from .apci import GroupValueRead, GroupValueResponse, GroupValueWrite
 from .telegram import Telegram, TelegramDirection
