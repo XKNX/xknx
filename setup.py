67a8afab77f9903541812200bd9863c312e80587
Marvin Wichmann <marvin.wichmann@unic.com>
2020-10-06 18:06:43 +0200

    Support Python 3.9

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=find_packages(),
     install_requires=REQUIRES,
