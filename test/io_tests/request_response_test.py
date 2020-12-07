886d0e704be4e9a279eab914e317552467a4ad28
farmio <farmio@alphart.net>
2020-12-07 20:23:02 +0100

    refactor timeout handling in RequestResponse

--- test/io_tests/request_response_test.py
+++ test/io_tests/request_response_test.py
@@ -1,10 +1,19 @@
 """Unit test for KNX/IP Disconnect Request/Response."""
 import asyncio
 import unittest
+from unittest.mock import MagicMock, patch
 
 from xknx import XKNX
 from xknx.io import RequestResponse, UDPClient
-from xknx.knxip import DisconnectResponse
+from xknx.knxip import DisconnectResponse, KNXIPBody
+
+
+class AsyncMock(MagicMock):
+    """Async Mock."""
+
+    # pylint: disable=invalid-overridden-method
+    async def __call__(self, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
 
 
 class TestConnectResponse(unittest.TestCase):
@@ -28,3 +37,37 @@ class TestConnectResponse(unittest.TestCase):
 
         with self.assertRaises(NotImplementedError):
             self.loop.run_until_complete(request_response.start())
+
+    @patch("logging.Logger.debug")
+    @patch("xknx.io.RequestResponse.send_request", new_callable=AsyncMock)
+    def test_request_response_timeout(self, _send_request_mock, logger_debug_mock):
+        """Test RequestResponse: timeout. No callback shall be left."""
+        xknx = XKNX()
+        udp_client = UDPClient(xknx, ("192.168.1.1", 0), ("192.168.1.2", 1234))
+        requ_resp = RequestResponse(xknx, udp_client, KNXIPBody)
+        requ_resp.response_received_or_timeout.wait = MagicMock(
+            side_effect=asyncio.TimeoutError()
+        )
+        self.loop.run_until_complete(requ_resp.start())
+        # Debug message was logged
+        logger_debug_mock.assert_called_once_with(
+            "Error: KNX bus did not respond in time (%s secs) to request of type '%s'",
+            1.0,
+            "RequestResponse",
+        )
+        # Callback was removed again
+        self.assertEqual(udp_client.callbacks, [])
+
+    @patch("xknx.io.RequestResponse.send_request", new_callable=AsyncMock)
+    def test_request_response_cancelled(self, _send_request_mock):
+        """Test RequestResponse: task cancelled. No callback shall be left."""
+        xknx = XKNX()
+        udp_client = UDPClient(xknx, ("192.168.1.1", 0), ("192.168.1.2", 1234))
+        requ_resp = RequestResponse(xknx, udp_client, KNXIPBody)
+        requ_resp.response_received_or_timeout.wait = MagicMock(
+            side_effect=asyncio.CancelledError()
+        )
+        with self.assertRaises(asyncio.CancelledError):
+            self.loop.run_until_complete(requ_resp.start())
+        # Callback was removed again
+        self.assertEqual(udp_client.callbacks, [])
