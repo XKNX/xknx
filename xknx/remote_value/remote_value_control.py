8a7e8575282d25cafcf5871fb7587f9f2d9bf304
buergi <patbuergi@gmx.de>
2020-12-10 21:58:05 +0100

    Port features from RemoteValueSensor to RemoteValueControl

--- xknx/remote_value/remote_value_control.py
+++ xknx/remote_value/remote_value_control.py
@@ -4,6 +4,8 @@ Module for managing a control remote value.
 Examples are switching commands with priority control, relative dimming or blinds control commands.
 DPT 2.yyy and DPT 3.yyy
 """
+from typing import List
+
 from xknx.dpt import DPTBase, DPTBinary
 from xknx.exceptions import ConversionError
 
@@ -18,20 +20,16 @@ class RemoteValueControl(RemoteValue):
         xknx,
         group_address=None,
         group_address_state=None,
+        sync_state=True,
         value_type=None,
         device_name=None,
+        feature_name="Control",
         after_update_cb=None,
         invert=False,
+        passive_group_addresses: List[str] = None,
     ):
         """Initialize control remote value."""
         # pylint: disable=too-many-arguments
-        super().__init__(
-            xknx,
-            group_address,
-            group_address_state,
-            device_name=device_name,
-            after_update_cb=after_update_cb,
-        )
         self.invert = invert
         _dpt_class = DPTBase.parse_transcoder(value_type)
         if _dpt_class is None:
@@ -39,6 +37,16 @@ class RemoteValueControl(RemoteValue):
                 "invalid value type", value_type=value_type, device_name=device_name
             )
         self.dpt_class = _dpt_class
+        super().__init__(
+            xknx,
+            group_address,
+            group_address_state,
+            sync_state=sync_state,
+            device_name=device_name,
+            feature_name=feature_name,
+            after_update_cb=after_update_cb,
+            passive_group_addresses=passive_group_addresses,
+        )
 
     def payload_valid(self, payload):
         """Test if telegram payload may be parsed."""
@@ -51,3 +59,13 @@ class RemoteValueControl(RemoteValue):
     def from_knx(self, payload):
         """Convert current payload to value."""
         return self.dpt_class.from_knx(payload.value, invert=self.invert)
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return self.dpt_class.unit
+
+    @property
+    def ha_device_class(self):
+        """Return a string representing the home assistant device class."""
+        return getattr(self.dpt_class, "ha_device_class", None)
