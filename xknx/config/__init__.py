6fbbd458feb87f32d00cc9183e225e24e181feb5
Marvin Wichmann <marvin@fam-wichmann.de>
2021-03-29 20:03:18 +0200

    Remove Config v2 (#644)
    * Remove Config v2
    
    * Review: Pylint & remove leftover

--- xknx/config/__init__.py
+++ xknx/config/__init__.py
@@ -2,38 +2,8 @@
 # flake8: noqa
 from .config import Config
 from .config_v1 import ConfigV1
-from .schema import (
-    BinarySensorSchema,
-    ClimateSchema,
-    ConnectionSchema,
-    CoverSchema,
-    DateTimeSchema,
-    ExposeSchema,
-    FanSchema,
-    LightSchema,
-    NotificationSchema,
-    SceneSchema,
-    SensorSchema,
-    SwitchSchema,
-    WeatherSchema,
-    XKNXSchema,
-)
 
 __all__ = [
     "Config",
     "ConfigV1",
-    "BinarySensorSchema",
-    "ClimateSchema",
-    "ConnectionSchema",
-    "CoverSchema",
-    "DateTimeSchema",
-    "ExposeSchema",
-    "FanSchema",
-    "LightSchema",
-    "NotificationSchema",
-    "SceneSchema",
-    "SensorSchema",
-    "SwitchSchema",
-    "WeatherSchema",
-    "XKNXSchema",
 ]
