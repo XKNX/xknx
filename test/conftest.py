1a2f5176a82736106fb174c10cdedbb18b52fde8
Matthias Alphart <farmio@alphart.net>
2021-08-08 21:44:43 +0200

    add time_travel fixture (#750)

---
+++ test/conftest.py
@@ -0,0 +1,50 @@
+"""Conftest for XKNX."""
+import asyncio
+
+import pytest
+
+
+class EventLoopClockAdvancer:
+    """Allow advancing of loop time."""
+
+    # thanks to @dermotduffy for his asyncio.sleep mock
+    # https://github.com/dermotduffy/hyperion-py/blob/main/tests/client_test.py#L273
+
+    __slots__ = ("offset", "loop", "_base_time")
+
+    def __init__(self, loop: asyncio.AbstractEventLoop) -> None:
+        """Initialize."""
+        self.offset = 0.0
+        self._base_time = loop.time
+        self.loop = loop
+
+        # incorporate offset timing into the event loop
+        self.loop.time = self.time  # type: ignore[assignment]
+
+    def time(self) -> float:
+        """Return loop time adjusted by offset."""
+        return self._base_time() + self.offset
+
+    async def _exhaust_callbacks(self) -> None:
+        """Run the loop until all ready callbacks are executed."""
+        while self.loop._ready:  # type: ignore[attr-defined]
+            await asyncio.sleep(0)
+
+    async def __call__(self, seconds: float) -> None:
+        """Advance time by a given offset in seconds."""
+        # Exhaust all callbacks.
+        await self._exhaust_callbacks()
+
+        if seconds > 0:
+            # advance the clock by the given offset
+            self.offset += seconds
+            # Once the clock is adjusted, new tasks may have just been
+            # scheduled for running in the next pass through the event loop
+            await asyncio.sleep(0)
+            await self._exhaust_callbacks()
+
+
+@pytest.fixture
+def time_travel(event_loop: asyncio.AbstractEventLoop) -> EventLoopClockAdvancer:
+    """Advance loop time and run callbacks."""
+    return EventLoopClockAdvancer(event_loop)
