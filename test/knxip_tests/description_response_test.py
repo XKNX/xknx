f518fa617b795f9c937425031ae2ae78e370bd58
farmio <farmio@alphart.net>
2022-01-23 09:01:27 +0100

    test with actual reply

--- test/knxip_tests/description_response_test.py
+++ test/knxip_tests/description_response_test.py
@@ -14,79 +14,8 @@ class TestKNXIPDescriptionResponse:
 
     def test_description_response(self):
         """Test parsing and streaming DescriptionResponse KNX/IP packet."""
-        raw = (
-            0x06,
-            0x10,
-            0x02,
-            0x04,
-            0x00,
-            0x48,
-            0x36,
-            0x01,
-            0x02,
-            0x00,
-            0x11,
-            0x00,
-            0x00,
-            0x00,
-            0x11,
-            0x22,
-            0x33,
-            0x44,
-            0x55,
-            0x66,
-            0xE0,
-            0x00,
-            0x17,
-            0x0C,
-            0x01,
-            0x02,
-            0x03,
-            0x04,
-            0x05,
-            0x06,
-            0x47,
-            0x69,
-            0x72,
-            0x61,
-            0x20,
-            0x4B,
-            0x4E,
-            0x58,
-            0x2F,
-            0x49,
-            0x50,
-            0x2D,
-            0x52,
-            0x6F,
-            0x75,
-            0x74,
-            0x65,
-            0x72,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x0C,
-            0x02,
-            0x02,
-            0x01,
-            0x03,
-            0x02,
-            0x04,
-            0x01,
-            0x05,
-            0x01,
-            0x07,
-            0x01,
+        raw = bytes.fromhex(
+            "061002040048360102001000000000082d40834de000170c000ab3274a324b4e582f49502d526f7574657200000000000000000000000000000000000c0202020302040205020701",
         )
         xknx = XKNX()
         knxipframe = KNXIPFrame(xknx)
@@ -100,9 +29,16 @@ class TestKNXIPDescriptionResponse:
         # done within knxip_dib_test.py
         assert isinstance(knxipframe.body.dibs[0], DIBDeviceInformation)
         assert isinstance(knxipframe.body.dibs[1], DIBSuppSVCFamilies)
-        assert knxipframe.body.device_name == "Gira KNX/IP-Router"
-        assert knxipframe.body.dibs[1].supports(DIBServiceFamily.ROUTING)
-        assert knxipframe.body.dibs[1].supports(DIBServiceFamily.TUNNELING)
+        assert knxipframe.body.device_name == "KNX/IP-Router"
+        assert knxipframe.body.dibs[1].supports(DIBServiceFamily.CORE, version=2)
+        assert knxipframe.body.dibs[1].supports(
+            DIBServiceFamily.DEVICE_MANAGEMENT, version=2
+        )
+        assert knxipframe.body.dibs[1].supports(DIBServiceFamily.ROUTING, version=2)
+        assert knxipframe.body.dibs[1].supports(DIBServiceFamily.TUNNELING, version=2)
+        assert knxipframe.body.dibs[1].supports(
+            DIBServiceFamily.REMOTE_CONFIGURATION_DIAGNOSIS
+        )
         assert not knxipframe.body.dibs[1].supports(DIBServiceFamily.OBJECT_SERVER)
 
         description_response = DescriptionResponse(xknx)
