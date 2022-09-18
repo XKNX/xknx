bf335e36246d648a8c0dc208b500562441ba5d84
farmio <farmio@alphart.net>
2022-09-19 00:26:08 +0200

    first tests

--- test/io_tests/routing_test.py
+++ test/io_tests/routing_test.py
@@ -1,11 +1,12 @@
 """Tests for KNX/IP routing connections."""
 import asyncio
-from unittest.mock import AsyncMock, call, patch
+from unittest.mock import AsyncMock, Mock, call, patch
 
 from xknx import XKNX
 from xknx.io import Routing
-from xknx.knxip import CEMIFrame
-from xknx.telegram import Telegram, TelegramDirection
+from xknx.io.routing import _RoutingFlowControl
+from xknx.knxip import CEMIFrame, KNXIPFrame, RoutingIndication
+from xknx.telegram import Telegram, TelegramDirection, tpci
 
 
 class TestRouting:
@@ -16,15 +17,15 @@ class TestRouting:
         # pylint: disable=attribute-defined-outside-init
         self.xknx = XKNX()
         self.tg_received_mock = AsyncMock()
-        self.routing = Routing(
+
+    @patch("xknx.io.Routing._send_knxipframe")
+    async def test_request_received_callback(self, send_knxipframe_mock):
+        """Test Routing for responding to a transport layer connection."""
+        routing = Routing(
             self.xknx,
             self.tg_received_mock,
             local_ip="192.168.1.1",
         )
-
-    @patch("xknx.io.Routing.send_telegram")
-    async def test_request_received_callback(self, send_telegram_mock):
-        """Test Tunnel for responding to L_DATA.req with confirmation and indication."""
         # L_Data.ind T_Connect from 1.0.250 to 1.0.255 (xknx tunnel endpoint)
         # communication_channel_id: 0x02   sequence_counter: 0x81
         raw_ind = bytes.fromhex("0610 0530 0010 2900b06010fa10ff0080")
@@ -34,19 +35,62 @@ class TestRouting:
         test_telegram = _cemi.telegram
         test_telegram.direction = TelegramDirection.INCOMING
 
-        response_telegram = Telegram(source_address=self.xknx.own_address)
+        response_telegram = Telegram(tpci=tpci.TDisconnect())
+        response_frame = KNXIPFrame.init_from_body(
+            RoutingIndication(
+                cemi=CEMIFrame.init_from_telegram(
+                    telegram=response_telegram, src_addr=self.xknx.own_address
+                )
+            )
+        )
 
         async def tg_received_mock(telegram):
             """Mock for telegram_received_callback."""
             assert telegram == test_telegram
             return [response_telegram]
 
-        self.routing.telegram_received_callback = tg_received_mock
-        self.routing.udp_transport.data_received_callback(
-            raw_ind, ("192.168.1.2", 3671)
-        )
+        routing.telegram_received_callback = tg_received_mock
+        routing.udp_transport.data_received_callback(raw_ind, ("192.168.1.2", 3671))
         await asyncio.sleep(0)
-        await asyncio.sleep(0)
-        assert send_telegram_mock.call_args_list == [
-            call(response_telegram),
+        assert send_knxipframe_mock.call_args_list == [
+            call(response_frame),
         ]
+
+
+class TestFlowControl:
+    """Test class for KNXnet/IP routing flow control."""
+
+    async def test_routing_busy(self, time_travel):
+        """Test throttling on received RoutingBusy frame."""
+        flow_control = _RoutingFlowControl()
+        mock = Mock()
+
+        async def test_send():
+            async with flow_control.throttle():
+                mock()
+
+        # first send is called immediately
+        task = asyncio.create_task(test_send())
+        await asyncio.sleep(0)
+        assert mock.call_count == 1
+        assert task.done()
+        mock.reset_mock()
+
+        # second send is throttled
+        task = asyncio.create_task(test_send())
+        await asyncio.sleep(0)
+        assert mock.call_count == 0
+        await time_travel(0.005)
+        assert not task.done()
+        await time_travel(0.015)
+        assert task.done()
+        assert mock.call_count == 1
+        mock.reset_mock()
+
+        # later send is called immediately
+        await time_travel(0.02)
+        task = asyncio.create_task(test_send())
+        await asyncio.sleep(0)
+        assert mock.call_count == 1
+        assert task.done()
+        mock.reset_mock()
