003e153456498d9ec29a0cb096f4fe1947606b6e
Pieter De Gendt <pieter.degendt@basalte.be>
2023-02-16 14:58:51 +0100

    Add Device Management Connection CRI/CRD tests
    Add tests for Request/Response connection for Device Management

--- test/knxip_tests/connect_response_test.py
+++ test/knxip_tests/connect_response_test.py
@@ -16,7 +16,7 @@ from xknx.telegram import IndividualAddress
 class TestKNXIPConnectResponse:
     """Test class for KNX/IP ConnectResponses."""
 
-    def test_connect_response(self):
+    def test_tunnel_connect_response(self):
         """Test parsing and streaming connection response KNX/IP packet."""
         raw = bytes.fromhex(
             "06 10 02 06 00 14 01 00 08 01 C0 A8 2A 0A 0E 57 04 04 11 FF"
@@ -42,6 +42,32 @@ class TestKNXIPConnectResponse:
 
         assert knxipframe2.to_knx() == raw
 
+    def test_mgmt_connect_response(self):
+        """Test parsing and streaming connection response KNX/IP packet."""
+        raw = bytes.fromhex("06 10 02 06 00 12 01 00 08 01 C0 A8 2A 0A 0E 57 02 03")
+        knxipframe, _ = KNXIPFrame.from_knx(raw)
+        assert isinstance(knxipframe.body, ConnectResponse)
+        assert knxipframe.body.communication_channel == 1
+        assert knxipframe.body.status_code == ErrorCode.E_NO_ERROR
+        assert knxipframe.body.data_endpoint == HPAI(ip_addr="192.168.42.10", port=3671)
+        assert (
+            knxipframe.body.crd.request_type
+            == ConnectRequestType.DEVICE_MGMT_CONNECTION
+        )
+        assert knxipframe.body.crd.individual_address == None
+
+        connect_response = ConnectResponse(
+            communication_channel=1,
+            status_code=ErrorCode.E_NO_ERROR,
+            data_endpoint=HPAI(ip_addr="192.168.42.10", port=3671),
+            crd=ConnectResponseData(
+                request_type=ConnectRequestType.DEVICE_MGMT_CONNECTION,
+            ),
+        )
+        knxipframe2 = KNXIPFrame.init_from_body(connect_response)
+
+        assert knxipframe2.to_knx() == raw
+
     def test_from_knx_wrong_crd(self):
         """Test parsing and streaming wrong ConnectRequest (wrong CRD length byte)."""
         raw = bytes.fromhex(
@@ -52,7 +78,25 @@ class TestKNXIPConnectResponse:
 
     def test_from_knx_wrong_crd2(self):
         """Test parsing and streaming wrong ConnectRequest (wrong CRD length)."""
-        raw = bytes.fromhex("06 10 02 06 00 14 01 00 08 01 C0 A8 2A 0A 0E 57 04 04")
+        raw = bytes.fromhex("06 10 02 06 00 12 01 00 08 01 C0 A8 2A 0A 0E 57 04 04")
+        with pytest.raises(CouldNotParseKNXIP):
+            KNXIPFrame.from_knx(raw)
+
+    def test_from_knx_wrong_crd3(self):
+        """Test parsing and streaming wrong ConnectRequest (CRD length too small)."""
+        raw = bytes.fromhex("06 10 02 06 00 12 01 00 08 01 C0 A8 2A 0A 0E 57 01 04")
+        with pytest.raises(CouldNotParseKNXIP):
+            KNXIPFrame.from_knx(raw)
+
+    def test_from_knx_wrong_crd4(self):
+        """Test parsing and streaming wrong ConnectRequest (wrong CRD length)."""
+        raw = bytes.fromhex("06 10 02 06 00 12 01 00 08 01 C0 A8 2A 0A 0E 57 04 03")
+        with pytest.raises(CouldNotParseKNXIP):
+            KNXIPFrame.from_knx(raw)
+
+    def test_from_knx_wrong_crd5(self):
+        """Test parsing and streaming wrong ConnectRequest (wrong CRD length)."""
+        raw = bytes.fromhex("06 10 02 06 00 13 01 00 08 01 C0 A8 2A 0A 0E 57 03 03 01")
         with pytest.raises(CouldNotParseKNXIP):
             KNXIPFrame.from_knx(raw)
 
